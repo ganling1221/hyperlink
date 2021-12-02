7575db19f7ca6bc66daa0e8e31e2430604458f09
Ling Gan <ganling1221@gmail.com>
2021-12-02 11:35:54 -0800

    message

--- hyperlink/src/hyperlink/SliderDemo.java
+++ hyperlink/src/hyperlink/SliderDemo.java
@@ -1,36 +1,5 @@
 package hyperlink;
 
-/*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
-
 
 import java.awt.*;
 import java.awt.event.*;
@@ -44,8 +13,7 @@ import javax.swing.*;
 import javax.swing.event.*;
 
 /*
- * SliderDemo.java requires all the files in the images/doggy
- * directory.
+ * A slider show for a given video
  */
 public class SliderDemo extends JPanel
                         implements ActionListener,
@@ -57,12 +25,12 @@ public class SliderDemo extends JPanel
 	private static final long serialVersionUID = 1L;
 	//Set up animation parameters.ç
     static final int FPS_MIN = 1;
-    static final int FPS_MAX = 29;
+    static final int FPS_MAX = 9000;
     static final int FPS_INIT = 15;    //initial frames per second
 	static int width = 352;
 	static int height = 288;
     int frameNumber = 0;
-    int NUM_FRAMES = 30;
+    int NUM_FRAMES = 9000;
     ImageIcon[] images = new ImageIcon[NUM_FRAMES];
     int delay;
     Timer timer;
@@ -71,9 +39,8 @@ public class SliderDemo extends JPanel
     //This label uses ImageIcon to show the doggy pictures.
     JLabel picture;
 
-    public SliderDemo(String[] paths) {
-    	video1Path = paths[0];
-        loadPicture();
+    public SliderDemo(String paths) {
+    	video1Path = paths;
 
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
@@ -92,13 +59,13 @@ public class SliderDemo extends JPanel
 
         //Turn on labels at major tick marks.
 
-        framesPerSecond.setMajorTickSpacing(10);
-        framesPerSecond.setMinorTickSpacing(1);
+        framesPerSecond.setMajorTickSpacing(1000);
+        framesPerSecond.setMinorTickSpacing(10);
         framesPerSecond.setPaintTicks(true);
         framesPerSecond.setPaintLabels(true);
         framesPerSecond.setBorder(
                 BorderFactory.createEmptyBorder(0,0,10,0));
-        Font font = new Font("Serif", Font.ITALIC, 15);
+        Font font = new Font("Serif", Font.ITALIC, 6);
         framesPerSecond.setFont(font);
 
         //Create the label that displays the animation.
@@ -193,21 +160,23 @@ public class SliderDemo extends JPanel
     /** Update the label to display the image for the current frame. */
     protected void updatePicture(int frameNumber) {
         //Set the image.
-        if (images[frameNumber] != null) {
-            picture.setIcon(images[frameNumber]);
-        } else { //image not found
-            picture.setText("image #" + frameNumber + " not found");
-        }
+    	String formatted = String.format("%04d", frameNumber+1);
+    	System.out.println(video1Path+ formatted + ".rgb");
+		ImageIcon image  = createImageIcon(video1Path+ formatted + ".rgb");
+		if(image != null) {
+            picture.setIcon(image);
+		}else {
+			System.out.println("No such image");
+		}
+        
     }
 
     private void loadPicture(){
     	//Get the image if we haven't already.
     	for(int i =0; i<NUM_FRAMES;i++) {
     		String formatted = String.format("%04d", i+1);
-    		System.out.println(formatted);
     		images[i] = createImageIcon(video1Path+ formatted + ".rgb");
     	}
-        
         
     }
     /** Returns an ImageIcon, or null if the path was invalid. */
@@ -273,10 +242,12 @@ public class SliderDemo extends JPanel
         JFrame frame = new JFrame("SliderDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //take in the first file path 
-        SliderDemo animator = new SliderDemo(args);
-                
+        SliderDemo animator = new SliderDemo(args[0]);
+        SliderDemo subVideo = new SliderDemo(args[1]);
+
         //Add content to the window.
-        frame.add(animator, BorderLayout.CENTER);
+        frame.add(animator, BorderLayout.WEST);
+        frame.add(subVideo, BorderLayout.EAST);
 
         //Display the window.
         frame.pack();
@@ -287,14 +258,11 @@ public class SliderDemo extends JPanel
     public static void main(String[] args) {
         /* Turn off metal's use of bold fonts */
         UIManager.put("swing.boldMetal", Boolean.FALSE);
-        
-        
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI(args);
-                
             }
         });
     }
